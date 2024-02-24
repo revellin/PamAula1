b95ef2532b03708bb9896e63d486088b61d3220e
Revellin Mendesz <99839050+revellin@users.noreply.github.com>
2024-02-23 22:14:34 -0300

    Add files via upload

---
+++ atv5/src/main/kotlin/Main.kt
@@ -0,0 +1,4 @@
+fun main(args: Array<String>) {
+    val aprovados = listOf("João" , "Maria" , "Pedro")
+   print ("O primeiro colocado fpo ${aprovados[0]}")
+}
---
+++ atv6/src/main/kotlin/Main.kt
@@ -0,0 +1,4 @@
+fun main(args: Array<String>) {
+    val bomHumor = true
+    print ("Hoje estou ${if (bomHumor) "feliz " else "chateado" } ")
+}
